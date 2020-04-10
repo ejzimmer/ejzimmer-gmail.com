7ce7868c3de001fd2178f64862fac75fbe481a0a
ejzimmer <ejzimmer@gmail.com>
2020-04-10 20:15:34 +1000

    hooray it works

--- handler.js
+++ handler.js
@@ -2,16 +2,26 @@ const ptv = require('ptv-api');
 
 const client = ptv(process.env.DEV_ID, process.env.KEY);
 
-module.exports.nextTrain = async event => {
-  const apis = await client;
-	const departures = await apis.Departures.Departures_GetForStopAndRoute({
-		stop_id: event.queryStringParameters.stop_id,
-		route_id: event.queryStringParameters.route_id,
-		route_type: 0
-	});
+exports.nextTrain = async event => {
+	const apis = await client;
+	
+	try {
+		const response = await apis.Departures.Departures_GetForStopAndRoute({
+			stop_id: event.queryStringParameters.stop_id,
+			route_id: event.queryStringParameters.route_id,
+			route_type: 0
+		});
 
-  return {
-    statusCode: 200,
-    body: departures
-  };
+		return {
+			statusCode: 200,
+			body: response.text
+		};
+	
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify(`something bad happened ${error}`)
+		}
+	}
+
 };
