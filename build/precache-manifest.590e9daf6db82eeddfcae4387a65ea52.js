e0695768d9462c570146d8fea7b5949874925f49
Erin Zimmer <erin@cogent.co>
2020-04-10 20:31:45 +1000

    generate ui

---
+++ build/precache-manifest.590e9daf6db82eeddfcae4387a65ea52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6440900fb2163a7964c9873482ff146b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8abbabd2da2bcaf09c9f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b9bbfc3e16a1852d938",
+    "url": "/static/js/2.482b7914.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.482b7914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8abbabd2da2bcaf09c9f",
+    "url": "/static/js/main.a339980c.chunk.js"
+  },
+  {
+    "revision": "437e95c65ea1669be2c6",
+    "url": "/static/js/runtime-main.6a2cc0c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
